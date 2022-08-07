38ae8dde1e5f781ff618d7a36a172191e48b6e01
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-08-07 17:01:23 +0300

    products component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Product } from 'src/app/models/product'
+import { ProductService } from 'src/app/services/product.service'
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  providers: [ProductService],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = []
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.productService
+        .getProducts(params['categoryId'])
+        .subscribe((data) => {
+          this.products = data
+        })
+    })
   }
-
 }
